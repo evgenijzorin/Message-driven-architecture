ca0609909d268f675b75034588f77058dd8b3d2f
“[Зорин <[evgenijzorin@bk.ru]>
2022-09-27 13:34:43 +0300

    homework for lesson 2 added

---
+++ Lesson_1/Seminar/Program.cs
@@ -0,0 +1,61 @@
+﻿using Seminar.Services;
+using Seminar.Services.impl;
+using System;
+using System.Diagnostics;
+
+namespace Seminar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Подключить кодирование текста UTF8
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            IMessenger _mesenger = new ConsoleMessager();
+            var rest = new Restaurant();
+            while(true)
+            {
+                // Request an action from the user
+                _mesenger.WriteMessage("Привет, желаете забронировать столик?\n1 - мы уведомим вас по смс (асинхронно)" +
+                    "\n2 - подождите на линии, мы вас оповестим (синхронно)" + "\n3 - Вывод информации по столикам\n");
+                if (!int.TryParse(Console.ReadLine(), out var choice)
+                    && choice is not (1 or 2 or 3))
+                {
+                    _mesenger.WriteMessage("Неверный ввод. Введите пожалуйста 1, 2 или 3\n");
+                    continue;
+                }       
+                var stopWatch = new Stopwatch();
+                stopWatch.Start(); // start timer                
+
+                // Обработка ответа пользователя:
+                switch (choice)
+                {
+                    case 1:
+                        _mesenger.WriteMessage("Введите колличество гостей");
+                        int.TryParse(Console.ReadLine(), out int countOfPersons);
+                        if(countOfPersons > 0)
+                        {
+                            rest.BookFreeTableAsync(countOfPersons);                            
+                        }
+                        break;
+                    case 2:
+                        _mesenger.WriteMessage("Введите колличество гостей");
+                        int.TryParse(Console.ReadLine(), out int countOfPersons1);
+                        if (countOfPersons1 > 0)
+                        {
+                            rest.BookFreeTable(countOfPersons1);
+                        }
+                        break;
+                    case 3:
+                        rest.ShowTablesStatus();
+                        break;
+                }
+                _mesenger.WriteMessage("Спасибо за ваше обращение!\n");
+                stopWatch.Stop();
+                var ts = stopWatch.Elapsed;
+                _mesenger.WriteMessage($"{ts.Seconds:00} : {ts.Milliseconds:00}"+ "\n"); // Show elapsed time
+            }  
+        }
+    }
+}
---
+++ Lesson_1/Seminar/Services/impl/ConsoleMessager.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Seminar.Services.impl
+{
+    public class ConsoleMessager : IMessenger
+    {
+        public ConsoleMessager()
+        {
+            // Подключить кодирование текста UTF8
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+        }
+
+        public void WriteMessage(string message) 
+        {
+            Console.Write(message);
+        }
+        /// <summary>
+        /// Write a delayed message
+        /// </summary>
+        /// <param name="message"></param>
+        /// <param name="seconds">number seconds of delay </param>
+        public void WriteMessage(string message, int seconds)
+        {
+            Thread.Sleep(1000 * seconds);
+            Console.Write(message);
+        }
+
+        public async Task WriteMessageAsync (string message, int seconds)
+        {
+            await Task.Delay(seconds);
+            Console.Write(message);
+        }
+    }
+}
